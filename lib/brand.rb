250a77155ef24f3141e93448ec14bc89de57b5c2
Nicko Dela Cruz <nickodc96@gamil.com>
2018-03-06 22:59:23 -0800

    changed method cap name

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,9 +1,9 @@
 class Brand < ActiveRecord::Base
   has_and_belongs_to_many(:stores)
   validates(:name, {:presence => true})
-  before_save(:capitalize_name)
+  before_save(:capitalize_letter)
 
-  def capitalize_name
+  def capitalize_letter
     self.name = name.split.map(&:capitalize).join(' ')
   end
 end
