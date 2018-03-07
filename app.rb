b5fe1f20c9011dbe8344406133e830122aa73d72
Nicko Dela Cruz <nickodc96@gamil.com>
2018-03-07 00:04:47 -0800

    add code to Brand_main page

--- app.rb
+++ app.rb
@@ -10,9 +10,22 @@ get('/') do
 end
 
 get('/brand_main') do
+  @brands = Brand.all
   erb(:brand_main)
 end
 
-get('/store_main') do
-  erb(:store_main)
+post('/brand_main') do
+  name = params['name']
+  price = (params['price'].to_i)*100
+  brand = Brand.find_or_create_by({:name => name, :price => price})
+  redirect("/brand_main/".concat(brand.id.to_s))
 end
+
+get('/brands/:id') do
+  @brand = Brand.find(params['id'].to_i)
+  erb(:brand)
+end
+
+# get('/store_main') do
+#   erb(:store_main)
+# end
