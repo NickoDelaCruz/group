604d822610b6f6e105068e5bc3dbb06ed534006b
Nicko Dela Cruz <nickodc96@gamil.com>
2018-03-07 01:01:25 -0800

    routing errors

--- app.rb
+++ app.rb
@@ -9,24 +9,6 @@ get('/') do
   erb(:home)
 end
 
-get('/brand_main') do
-  @brands = Brand.all
-  erb(:brand_main)
-end
-
-post('/brand_main') do
-  name = params['name']
-  price = (params['price'].to_i)*100
-  brand = Brand.find_or_create_by({:name => name, :price => price})
-  redirect("/brand_main/".concat(brand.id.to_s))
-end
-
-get('/brand_main/:id') do
-  @brand = Brand.find(params['id'].to_i)
-  erb(:brand)
-end
-
-
 get('/store_main') do
   @stores = Store.all
   erb(:store_main)
@@ -47,4 +29,45 @@ end
 get('/store_main/:id/edit') do
   @store = Store.find(params['id'].to_i)
   erb(:store_edit)
+end
+
+patch('/store_main/:id') do
+  name = params['name']
+  @store = Store.find(params['id'].to_i)
+  @store.update({:name => name})
+  @stores = Store.all
+  @brands = Brand.all
+  erb(:store)
+end
+
+delete('/store_main/:id') do
+  @store = Store.find(params['id'].to_i)
+  @store.destroy
+  @stores = Store.all
+  @brands = Brand.all
+  erb(:store_main)
+end
+
+get('/brand_main') do
+  @brands = Brand.all
+  erb(:brand_main)
+end
+
+post('/brand_main') do
+  name = params['name']
+  price = (params['price'].to_i)*100
+  brand = Brand.find_or_create_by({:name => name, :price => price})
+  redirect("/brand_main/".concat(brand.id.to_s))
+end
+
+get('/brand_main/:id') do
+  @brand = Brand.find(params['id'].to_i)
+  erb(:brand)
+end
+
+post('/store_main/:id/brands') do
+  brand = Brand.find(params['brand'].to_i)
+  store = Store.find(params['id'].to_i)
+  store.brands << brand unless store.brands.include?(brand)
+  redirect("/store_main/".concat(store.id.to_s))
 end
